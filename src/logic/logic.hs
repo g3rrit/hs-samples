ae334cf83eba5e818e9186ab5325e5c2e95ef827
pearisgreen <gerrit.proessl@gmail.com>
2019-01-29 23:08:15 +0100

    update

--- src/logic/logic.hs
+++ src/logic/logic.hs
@@ -1,15 +1,35 @@
+import Text.ParserCombinators.Parsec
 
 data L_Op =
-  Op Bool
+  Op Char
   | And_Op L_Op L_Op
   | Or_Op L_Op L_Op
   | Not_Op L_Op
+  | Impl_Op L_Op L_Op
 
-l_eval :: L_Op -> Bool
-l_eval (Op b) = b
-l_eval (And_Op l r) = l_eval l && l_eval r
-l_eval (Or_Op l r) = l_eval l || l_eval r
-l_eval (Not_Op u) = not $ l_eval u
+l_eval :: L_Op -> (Char -> Bool) -> Bool
+l_eval (Op c) f = f c
+l_eval (And_Op l r) f = l_eval l f && l_eval r f
+l_eval (Or_Op l r) f = l_eval l f || l_eval r f
+l_eval (Not_Op u) f = not $ l_eval u f
+l_eval (Impl_Op l r) f = not $ l_eval l f && l_eval r f
+
+test = And_Op (Op 'a') (Op 'a')
+res = l_eval test (\a -> case a of
+                           'a' -> True
+                           'b' -> False)
+
+exp :: GenParser Char st (L_Op, [Char])
+exp = lop eol
+
+lop :: GenParser Char st (L_Op, [Char])
+lop = and_op <|> or_op <|> not_op <|> impl_op
+
+and_op :: GenParser Char st {L_Op, [Char])
+and_op = string "and" >> tuple
 
 
-test = Not_Op ( And_Op ( Op True) ( Op False))
+-- create_char_list :: [Char] -> [(Char, Bool)]
+
+
+
