22a5070fad92d7f4e00ef97057e233134a3c520b
pearisgreen <gerrit.proessl@gmail.com>
2019-01-31 11:57:00 +0100

    update

--- src/logic/logic.hs
+++ src/logic/logic.hs
@@ -14,11 +14,36 @@ l_eval (Or_Op l r) f = l_eval l f || l_eval r f
 l_eval (Not_Op u) f = not $ l_eval u f
 l_eval (Impl_Op l r) f = not $ l_eval l f && l_eval r f
 
+
+-- get character table for evaluation
+char_row :: [Char] -> Int -> [(Char, Bool)]
+char_row c i 
+  | s < 0 = []
+  | otherwise = [(cc, t)] ++ char_row (drop 1 c) (if t then res else i)
+    where s = (length c) - 1
+          cc = head c
+          res = i - 2^s
+          t = res >= 0
+
+char_table' :: [Char] -> Int -> [[(Char, Bool)]]
+char_table' a i 
+  | i < 0 = []
+  | otherwise = [(char_row a i)] ++ (char_table' a (i - 1))
+                         
+char_table :: [Char] -> [[(Char, Bool)]]
+char_table a = char_table' a (2^(length a) - 1)
+--------------------------------------
+
+
 test = And_Op (Op 'a') (Op 'a')
 res = l_eval test (\a -> case a of
                            'a' -> True
                            'b' -> False)
 
+
+
+
+{--
 exp :: GenParser Char st (L_Op, [Char])
 exp = lop eol
 
@@ -27,7 +52,7 @@ lop = and_op <|> or_op <|> not_op <|> impl_op
 
 and_op :: GenParser Char st {L_Op, [Char])
 and_op = string "and" >> tuple
-
+--}
 
 -- create_char_list :: [Char] -> [(Char, Bool)]
 
