dc5762c080963b53bec08ab1657c0e76155d0eeb
pearisgreen <gerrit.proessl@gmail.com>
2019-06-01 19:15:59 +0200

    # Update unificator

--- src/unificator/unificator.hs
+++ src/unificator/unificator.hs
@@ -1,4 +1,6 @@
 import qualified Data.Set as Set
+import qualified Data.Char as Char
+import qualified Data.Foldable as F
 
 data Term = Fn Char [Term] | Var Char deriving Show
 
@@ -15,7 +17,18 @@ instance Ord Term where
                                          (Fn sn _, Fn tn _) -> compare sn tn
                                          (Var sn, Var tn) -> compare sn tn
 
-type Unificator = Set.Set (Char, Term)
+data Rule = Rule Char Term deriving Show
+
+instance Eq Rule where
+  (==) (Rule a b) (Rule x y) = a == x
+
+instance Ord Rule where
+  compare (Rule a b) (Rule x y) = compare a x
+
+type Unificator = Set.Set Rule
+
+
+-- -------- UNIFICATOR_ALG-------- --
 
 vars :: Term -> Set.Set Char
 vars t = case t of
@@ -26,13 +39,68 @@ compare_arity_sym :: Term -> Term -> Bool
 compare_arity_sym (Fn sn st) (Fn tn tt) = (sn == tn) && ((length st) == (length tt))
 compare_arity_sym _ _ = False
 
-unify :: Term -> Term -> Maybe Unificator
-unify s t = if s == t then Just (Set.empty) else
+get_unificator :: Term -> Term -> Maybe Unificator
+get_unificator s t = if s == t then Just (Set.empty) else
               case (s, t) of
-                (Var sn, _)          -> if (Set.member sn (vars t)) then Nothing else Just (Set.singleton (sn, t))
-                (_, Var _)           -> unify t s
+                (Var sn, _)          -> if (Set.member sn (vars t)) then Nothing else Just (Set.singleton (Rule sn t))
+                (_, Var _)           -> get_unificator t s
                 (Fn sn st, Fn tn tt) -> if (compare_arity_sym s t) then foldr (\(a, b) x -> uadd a b x) (Just Set.empty) (zip st tt) else Nothing
   where uadd l m x = do
           x' <- x
-          h  <- unify l m
+          h  <- get_unificator (apply_unificator l x') (apply_unificator m x')
           return (Set.union x' h)
+
+apply_unificator :: Term -> Unificator -> Term
+apply_unificator t u = case t of
+  Fn n tx -> Fn n (map (`apply_unificator` u) tx)
+  Var n -> let r = (F.find (\(Rule a b) -> a == n) u) in
+             case r of
+               Nothing -> Var n
+               Just (Rule a b) -> b
+
+-- -------- PARSER-------- --
+
+parse_term :: String -> Maybe (Term, String)
+parse_term [] = Nothing
+parse_term (x:xs) = case x of
+  a | Char.isAlpha a -> Just (Var a, xs)
+  '(' -> parse_fn xs
+  ' ' -> parse_term xs
+  _ -> Nothing
+
+parse_fn :: String -> Maybe (Term, String)
+parse_fn [] = Nothing
+parse_fn (x:xs) = case x of
+  a | Char.isAlpha a -> let args = parse_fn_args xs in
+                     case args of
+                       Just (args', xs') -> Just (Fn a args', xs')
+                       Nothing -> Nothing
+  ' ' -> parse_fn xs
+  _ -> Nothing
+
+parse_fn_args :: String -> Maybe ([Term], String)
+parse_fn_args [] = Nothing
+parse_fn_args (x:xs) = case x of
+  x | x == ')' -> Just ([], xs)
+  x | x == ' ' -> parse_fn_args xs
+  _ -> let r = parse_term (x:xs) in
+         case r of
+           Just (t, xs') -> let rs = parse_fn_args xs' in
+                              case rs of
+                                Just (rs', xss) -> Just (t : rs', xss)
+                                Nothing -> Nothing
+           Nothing -> Nothing
+
+-- -------- MAIN-------- --
+
+main = do
+  putStr "Enter term s: "
+  s <- getLine
+  putStr "Enter term t: "
+  t <- getLine
+  case (parse_term s, parse_term t) of
+    (Just (s', _), Just (t', _)) -> do
+      putStrLn ("s : (" ++ (show s') ++ ")")
+      putStrLn ("t : (" ++ (show t') ++ ")")
+      putStrLn ("unificator: " ++ (show (get_unificator s' t')))
+    _ -> putStrLn "Error on input"
